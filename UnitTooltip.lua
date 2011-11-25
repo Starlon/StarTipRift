3b4a438d665436f2541692b6f267f54ca7299705
Scott Sibley <sisibley@gmail.com>
2011-11-24 22:08:24 -0600

    Slight modifications.

--- UnitTooltip.lua
+++ UnitTooltip.lua
@@ -7,6 +7,16 @@ local Evaluator = LibStub("LibScriptableUtilsEvaluator-1.0")
 local tinsert = table.insert
 local tremove = table.remove
 
+local lines = {}
+local config = {
+	onMouseover = {
+		code = [[
+			ResetDPS(unit)
+		]]
+
+	}
+}
+
 local function copy(src, dst)
     if type(src) ~= "table" then return nil end
     if type(dst) ~= "table" then dst = {} end
@@ -19,247 +29,7 @@ local function copy(src, dst)
     return dst
 end
 
-local lines = {}
-local config = {
 
-onMouseover = {
-	code = [[
-ResetDPS(unit)
-]]
-},
-
-lines = {
-    [1] = {
-		id = "unitname",
-        name = "UnitName",
-        left = [[
-local name = UnitName(unit)
-if not name then return end
-name = name .. (UnitPVP(unit) and "<PVP>" or "")
-local afk = UnitAFK(unit)
-local afk_time = UnitAFKTime(unit)
-local afk_fmt = afk and (afk_time and Angle('AFK: ' .. FormatDuration(afk_time)) or Angle('AFK')) or ''
-local offline = UnitOffline(unit)
-local offline_time = UnitOfflineTime(unit)
-local offline_fmt = offline and (offline_time and Angle('Offline: ' .. FormatDuration(offline_time)) or Angle('Offline')) or ""
-if name then
-	return name .. afk_fmt .. offline_fmt
-end
-]],
-        colorLeft = [[
-return UnitRelationColor(unit)
-]],
-        enabled = true,
-		update = 1000,
-		leftUpdating = true,
-		fontSize = 15
-    },
-	[2] = {
-		id = "target",
-		name = "Target",
-		left = "return 'Target:'",
-		right = [[
-local pvp = UnitPVP(unit .. ".target") and "++" or ""
-local name = UnitName(unit..".target")
-return  name and (name .. pvp) or "None"
-]],
-		colorRight = [[
-if not UnitName(unit..".target") then return 1, 1, 1, 1 end
-return UnitRelationColor(unit..'.target')
-]],
-		rightUpdating = true,
-		update = 500,
-		--alignRight = WidgetText.ALIGN_RIGHT,
-		enabled = true,
-	},
-	[3] = {
-		id = "guild",
-		name = "Guild/Title",
-		left = "return UnitPlayer(unit) and 'Guild:' or 'Title:'",
-		right = [[
-if UnitPlayer(unit) then
-	local guild = UnitGuild(unit)
-	return guild and Angle(guild)
-else
-	local title = UnitNameSecondary(unit)
-	return title and Angle(title)
-end
-]],
-		enabled = true
-	},
-	[4] = {
-		id = "level",
-		name = "Level",
-		left = "return 'Level:'",
-		right = "return UnitLevel(unit)",
-		colorRight = "return DifficultyColor(unit)",
-		enabled = true
-	},
-	[5] = {
-		id = "calling",
-		name = "Calling",
-
-		left = [[
-return "Calling:"
-]],
-		right = [[
-return UnitCalling(unit)
-]],
-		
-		enabled = true
-	},
-	[6] = {
-		id = "role",
-		name = "Role",
-		left = "return 'Role:'",
-		right = "return UnitRole(unit)",
-		enabled = true
-	},
-	[7] = {
-		id = "faction",
-		name = "Faction",
-		left ="return 'Faction:'",
-		right = "return UnitFaction(unit)",
-		enabled = true
-	},
-	[8] = {
-		id = "relation",
-		name = "Relation",
-		left = "return 'Relation:'",
-		right = "return UnitRelation(unit)",
-		enabled = true	
-	},
-	[9] = {
-		id = "health",
-		name = "Health",
-		left = "return 'Health:'",
-		right = [[
-if not UnitHealth(unit) then return end
-return Short(UnitHealth(unit), true) .. '/' .. Short(UnitHealthMax(unit), true)
-]],
-		colorRight = [[
-if not UnitHealth(unit) then return end
-return GradientHealth(UnitHealth(unit) / UnitHealthMax(unit))	
-]],
-		rightUpdating = true,
-		update = 200,
-		cols = 15,
-		enabled = true
-	},
-	[10] = {
-		id = "mana",
-		name = "Mana",
-		left = "return 'Mana:'",
-		right = [[
-if not UnitMana(unit) then return end
-return Short(UnitMana(unit), true) .. '/' .. Short(UnitManaMax(unit), true)
-]],
-		colorRight = [[
-if not UnitMana(unit) then return end
-return GradientMana(UnitMana(unit) / UnitManaMax(unit))
-]],
-		rightUpdating = true,
-		update = 200,
-		cols = 15,
-		enabled = true
-	},
-	[11] = {
-		id = "power",
-		name = "Power",
-		left = "return 'Power:'",
-		right = [[
-return UnitPower(unit)
-]],
-		colorRight = [[
-if not UnitPower(unit) then return end
-return GradientMana(UnitPower(unit) / 100)
-]],
-		rightUpdating = true,
-		update = 200,
-		cols = 15,
-		enabled = true
-	},
-	[12] = {
-		id = "energy",
-		name = "Energy",
-		left = "return 'Energy:'",
-		right = [[
-return UnitEnergy(unit)
-]],
-		colorRight = [[
-if not UnitEnergy(unit) then return end
-return GradientMana(UnitEnergy(unit) / UnitEnergyMax(unit))
-]],
-		rightUpdating = true,
-		update = 200,
-		cols = 15,
-		enabled = true
-	},	
-	[13] = {
-		id = "guaranteedloot",
-		name = "Guaranteed Loot",
-		left = "return UnitGuaranteedLoot(unit) and Angle('This NPC is guaranteed to drop loot.')",
-		enabled = true
-	},	
-	[14] = {
-		id = "loot",
-		name = "Loot",
-		left = "return 'Loot:'",
-		right = [[
-local loot = UnitLoot(unit)
-if loot then return UnitName(loot) end
-]],
-		enabled = true
-	},
-	[15] = {
-		id = "mark",
-		name = "Mark",
-		left = "return 'Mark:'",
-		right = "return UnitMark(unit)",
-		enabled = true
-	},
-	[16] = {
-		id = "race",
-		name = "Race",
-		left = "return 'Race:'",
-		right = "return UnitRace(unit)",
-		enabled = true
-	},
-	[17] = {
-		id = "location",
-		name = "Location",
-		left = "return 'Location:'",
-		right = "return UnitLocation(unit)",
-		enabled = true
-	},
-	[18] = {
-		id = 'tag',
-		name = "Tag",
-		left = [[
-return UnitTagText(unit)
-]],
-		enabled = true
-	},
-	[19] = {
-		id = "publicsize",
-		name = "Public Size",
-		left = "local pg = UnitPublicSize(unit); if pg then return 'Public Group (' ..pg..')'; end ",
-		enabled = true
-
-	},
-	[20] = {
-		id = "dps",
-		name = "DPS",
-		left = "return 'DPS:'",
-		right = "return UnitDPS(unit) or '---'",
-		enabled = true,
-		rightUpdating = true,
-		update = 200
-	}
-
-}
-}
-lines = {}
 local function wipe(tbl)
 	for i = 1, #tbl do
 		tremove(tbl)
@@ -276,7 +46,7 @@ do
     function draw()
         for i, widget in ipairs(widgetsToDraw) do
 			if widget.cell and widget.buffer ~= "" then
-				widget.cell:SetFontSize(widget.fontSize or 12)
+				widget.cell:SetFontSize(widget.config.fontSize or 12)
 				widget.cell:SetText(widget.buffer)
 			end
 
