346f6a6f10cd332589ee2c8539db5ea5ed070531
Scott Sibley <sisibley@gmail.com>
2011-12-11 16:38:14 -0600

    Allow configuring of animation settings individually.

--- Animation.lua
+++ Animation.lua
@@ -8,7 +8,7 @@ local LibCore = LibStub("LibScriptableLCDCoreLite-1.0")
 mod.environment = {}
 mod.core = LibCore:New(mod.environment, "Animation")
 
-local animation = {
+mod.animation = {
 		animationsOn = false,
 		animationSpeed = 1000,
 		animationInit = [[
@@ -25,20 +25,20 @@ d=(v*0.3); r=t+i*PI*0.02; x=cos(r)*d; y=sin(r)*d
 }
 
 function mod:RunFrame()
-	Evaluator.ExecuteCode(mod.environment, "StarTip.Position.animationBegin", animation.animationBegin)
+	Evaluator.ExecuteCode(mod.environment, "StarTip.Position.animationBegin", self.animation.animationBegin)
 end
 
 function mod:OnStartup()
 end
 
 function mod:RunInit()
-	Evaluator.ExecuteCode(self.environment, "StarTip.Position.animationIni", animation.animationInit)
+	Evaluator.ExecuteCode(self.environment, "StarTip.Position.animationIni", self.animation.animationInit)
 end
 
 local random, floor = math.random, math.floor
 function mod:RunPoint(x, y)
 	local x, y = x or 0, y or 0
-	if animation.animationsOn then
+	if self.animation.animationsOn then
 		mod.environment.i = (mod.environment.i or 0) + 1
 		mod.environment.v = (mod.environment.v or 0) +  random()
 		mod.environment.x = x
@@ -47,12 +47,12 @@ function mod:RunPoint(x, y)
 		mod.environment.height = StarTip.tooltipMain.frame:GetHeight()
 		mod.unit = StarTip.unit
 		mod.self = mod.self or {}
-		Evaluator.ExecuteCode(mod.environment, "Position.animationPoint", animation.animationPoint)
+		Evaluator.ExecuteCode(mod.environment, "Position.animationPoint", self.animation.animationPoint)
 
 		local xx, yy = mod.environment.x or 0, mod.environment.y or 0
 		if mod.environment.gravity then
-		        x = x + floor((((xx or 0) + 1.0) * UIParent:GetWidth() / animation.animationSpeed))
-        		y = y + floor((((yy or 0) + 1.0) * UIParent:GetHeight() / animation.animationSpeed))
+		        x = x + floor((((xx or 0) + 1.0) * UIParent:GetWidth() / self.animation.animationSpeed))
+        		y = y + floor((((yy or 0) + 1.0) * UIParent:GetHeight() / self.animation.animationSpeed))
 		else
 			x = xx
 			y = yy
@@ -66,6 +66,11 @@ function mod:SetUnit()
 end
 
 function mod:Establish(anim)
-	animation = anim or animation
+	self.animation.animationsOn = anim and anim.animationsOn or self.animation.animationsOn
+	self.animation.animationSpeed = anim and anim.animationSpeed or self.animation.animationSpeed
+	self.animation.animationInit = anim and anim.animationInit or self.animation.animationInit
+	self.animation.animationBegin = anim and anim.animationBegin or self.animation.animationBegin
+	self.animation.animationPoint = anim and anim.animationPoint or self.animation.animationPoint
 	self:RunInit()
+print(self.animation.animationsOn)
 end
